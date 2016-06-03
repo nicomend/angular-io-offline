5567545443e2080236c97de54a500fd877e79552
Nico <nicom93@gmail.com>
2016-06-03 16:24:25 +0300

    initial commit

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ let PORT = 9999;
 app.use(express.static('www'));
 
 app.listen(PORT, () => {
-  console.log(`running angular.io docs on http://localhost:${PORT}`);
+  console.log(`running angular.io on http://localhost:${PORT}`);
   console.log(`built by Nico :)`);
 });
